9154d390e0539d4ae2ccd384bd1e4850925a1984
Melike <22410051025@ogr.erbakan.edu.tr>
2025-03-03 23:50:38 +0300

    create action method

--- Controllers/CourseController.cs
+++ Controllers/CourseController.cs
@@ -4,5 +4,11 @@ namespace dotnet_basics.Controllers;
 
     public class CourseController : Controller
     {
-        
+        //localhost:5052/course
+        //localhost:5052/course/list
+
+        public string  List()
+        {
+            return "ben list sayfasıyım";
+        }
     }
--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,5 +4,18 @@ namespace dotnet_basics.Controllers;
 
 public class HomeController:Controller //Controller daki özelliklerin HomeController a geçmesini sağladık.
 {
+    //localhost:5052/home
+    //localhost:5052/home/about
+    //localhost:5052/home/contact
+    //localhost:5052/home/detail
 
+    public string About()
+    {
+        return "about sayfasıyım";
+    }
+
+    public string Contact()
+    {
+        return "ben contact sayfasıyım";
+    }
 }
