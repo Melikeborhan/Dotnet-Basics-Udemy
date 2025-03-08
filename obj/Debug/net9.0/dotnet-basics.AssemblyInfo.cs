5868d7b0086fa25e02a76d0988aff658fb383c88
Melike <22410051025@ogr.erbakan.edu.tr>
2025-03-08 13:23:00 +0300

    Created _ViewStart and implemented layout coding

--- obj/Debug/net9.0/dotnet-basics.AssemblyInfo.cs
+++ obj/Debug/net9.0/dotnet-basics.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-basics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82aa7995d2ce7c6eff3e4d563d3ac06ef9a02f83")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eda0282876b2614945f11b4ae44e8ace4cd37b5c")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-basics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-basics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
