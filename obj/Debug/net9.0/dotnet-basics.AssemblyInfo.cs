34572665cda541b6035f4d68433858fc8d681c64
Melike <22410051025@ogr.erbakan.edu.tr>
2025-03-08 14:39:44 +0300

    static files-created img files and used bootstrap from wwwroot

--- obj/Debug/net9.0/dotnet-basics.AssemblyInfo.cs
+++ obj/Debug/net9.0/dotnet-basics.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-basics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e8c17fe094258fcc1ffa14a345c6095c942346a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca0d1542514450d8ef2bf5848e4b8038f031eb04")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-basics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-basics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
