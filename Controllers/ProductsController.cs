f5500e1a411e7802fadec403fcef1be5c50e36ee
Melike <22410051025@ogr.erbakan.edu.tr>
2025-03-16 17:06:25 +0300

    created one product and Viewed in the page

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -14,6 +14,27 @@ namespace dotnet_basics.Controllers;
         }
 
         public ActionResult Details(){
+
+            //Samsung S24 Ultra
+            //Samsung S24 Ultra 512 GB SSD 16 GB RAM
+            //70.000 TL
+            //tel-1.jpg
+
+            string productName = "Samsung S24 Ultra";
+            string productDetail = "Samsung S24 Ultra 512 GB SSD 16 GB RAM";
+            double productPrice =  70000 ;
+            string productImage = "tel-1.jpg";
+            bool  productIsStock = true;
+
+
+            ViewData["ProductName"] = productName;
+            ViewData["ProductDetail"] = productDetail;
+            ViewData["ProductPrice"] = productPrice;
+            ViewData["ProductImage"] = productImage;
+            ViewData["ProductIsStock"] = productIsStock;
+
+
+
             return View();
         }
 
