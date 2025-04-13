4fc6b573d9e9c92dbf39bc40230e3146230f847a
Melike <22410051025@ogr.erbakan.edu.tr>
2025-04-13 15:51:59 +0300

    fixed uı

--- obj/Debug/net9.0/dotnet-basics.AssemblyInfo.cs
+++ obj/Debug/net9.0/dotnet-basics.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-basics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+279aedfbc81386f17ca5e4327d3451f2ecc4f8e6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f224a947d847d405125476c74ffc14cd38376ee2")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-basics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-basics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
