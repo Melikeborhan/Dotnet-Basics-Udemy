4dd6b67c3cd963aff9f3c7efd37d669593398388
Melike <22410051025@ogr.erbakan.edu.tr>
2025-04-13 19:27:09 +0300

    fixed ui card

--- obj/Debug/net9.0/dotnet-basics.AssemblyInfo.cs
+++ obj/Debug/net9.0/dotnet-basics.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("dotnet-basics")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fc6b573d9e9c92dbf39bc40230e3146230f847a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+950b73d5247749d101ac2e294c0c10f32abc58ad")]
 [assembly: System.Reflection.AssemblyProductAttribute("dotnet-basics")]
 [assembly: System.Reflection.AssemblyTitleAttribute("dotnet-basics")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
